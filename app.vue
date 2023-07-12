03ff9a7582c9229f29018283a2cec0537dcbee60
cherbourg@fub.fr <cherbourg@fub.fr>
2023-07-13 00:02:16 +0200

    Initial commit

---
+++ app.vue
@@ -0,0 +1,21 @@
+<template>
+  <body>
+  <Header/>
+  </body>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+
+body {
+  background-color: #282C35 ;
+  width: 100%;
+  height: 100%;
+}
+
+
+
+
+</style>
---
+++ components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+
+<div class="container">
+        <h1>Overbooks</h1>
+</div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@800&display=swap');
+h1 {
+    color: white;
+    font-size: 42px;
+    font-family: 'Montserrat', sans-serif;
+}
+
+
+.container {
+  display: flex;
+  align-items: center;
+}
+
+</style>
